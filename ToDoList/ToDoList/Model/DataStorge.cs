d2659d603ab55c7efd741729437e49d4b41acb90
emiliano scolaro <escolaroo@gmail.com>
2019-10-28 21:59:03 -0300

    Agregar archivos de proyecto.

---
+++ ToDoList/ToDoList/Model/DataStorge.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace ToDoList.Model
+{
+    
+    public class DataStorge
+    {
+        private ObservableCollection<Task> Task;
+
+        public DataStorge()
+        {
+            Task = new ObservableCollection<Task>();
+        }
+
+        public void Add(string nombre)
+        {
+            Task.Add(new Task { Name = nombre });
+        }
+
+
+        public ObservableCollection<Task> Tasks
+        {
+            get { return Tasks; }
+
+        }
+
+
+    }
+
+    
+}
---
+++ ToDoList/ToDoList/Model/Task.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList.Model
+{
+    public class Task
+    {
+        public string Name { get; set; }
+        public bool estado { get; set; }
+    }
+}
